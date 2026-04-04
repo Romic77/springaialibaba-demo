58919ff6f73726e8aa81b90d04bcf8a90d0b9a8e
Romic <280385556@qq.com>
2026-04-04 11:59:57 +0800

    feat(ai): 添加基于Ollama的AI聊天功能模块
    - 新增AiConfig配置类用于创建ChatClient Bean
    - 配置application.yml和application-dev.yml文件支持Ollama服务- 添加HelloworldController实现简单聊天和流式聊天接口
    - 创建saa-chatclientchatmodel模块并配置相关依赖
    - 更新项目编码配置支持新模块的UTF-8编码- 添加Spring Boot启动类并实现命令行运行器

---
+++ chat-prompt/src/main/java/com/ai/controller/PromptController.java
@@ -0,0 +1,49 @@
+package com.ai.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.util.Objects;
+
+@RestController
+public class PromptController {
+    @Autowired
+    private ChatClient chatClient;
+    @Autowired
+    private OllamaChatModel chatModel;
+
+
+    @GetMapping("/prompt/chat")
+    public Flux<String> chat(@RequestParam("query") String query) {
+        //垂直ai能力边界
+        return chatClient.prompt().system("你是一个法律助手，其他问题回复无可奉告。").user(query).stream().content();
+    }
+
+    @GetMapping("/prompt/chat2")
+    public Flux<String> chat2(@RequestParam("query") String query) {
+        SystemMessage systemMessage = new SystemMessage("讲个笑话，100字以内");
+
+        UserMessage userMessage = new UserMessage(query);
+
+        Prompt prompt = new Prompt(userMessage, systemMessage);
+
+        return chatModel.stream(prompt)
+                .mapNotNull(resp -> resp.getResult().getOutput().getText());
+    }
+
+    @GetMapping("/prompt/chat3")
+    public String chat3(@RequestParam("query") String query) {
+        return Objects.requireNonNull(chatClient.prompt().user(query).call().chatResponse()).getResult().getOutput().getText();
+    }
+}
